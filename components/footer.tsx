9d07f727fa970c820633420b15235fd1f83e6667
Denyme24 <namanraj24@outlook.com>
2025-01-15 14:29:02 +0530

    added linkedin url

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,19 +1,19 @@
-import Link from "next/link";
-import { Linkedin, Github } from "lucide-react";
+import Link from 'next/link';
+import { Linkedin, Github } from 'lucide-react';
 
 export default function Footer() {
   const socialLinks = [
     {
-      name: "LinkedIn",
+      name: 'LinkedIn',
       icon: Linkedin,
-      href: "https://www.linkedin.com/in/naman-raj24",
+      href: 'https://www.linkedin.com/in/namanraj24/',
     },
-    { name: "Github", icon: Github, href: "https://github.com/Denyme24" },
+    { name: 'Github', icon: Github, href: 'https://github.com/Denyme24' },
   ];
 
   return (
-    <footer className="border-t border-gray-200 py-8 px-6 ">
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row md:items-center justify-between gap-8">
+    <footer className="border-t border-gray-200 px-6 py-8">
+      <div className="mx-auto flex max-w-7xl flex-col justify-between gap-8 md:flex-row md:items-center">
         {/* Copyright Section */}
         <div className="text-sm text-gray-600">
           <p>© 2024 by Naman Raj.</p>
@@ -22,11 +22,11 @@ export default function Footer() {
         {/* Contact Section */}
         <div className="flex gap-12">
           <div>
-            <h3 className="font-medium mb-2">Call</h3>
+            <h3 className="mb-2 font-medium">Call</h3>
             <p className="text-sm text-gray-600">+91 7321896070</p>
           </div>
           <div>
-            <h3 className="font-medium mb-2">Write</h3>
+            <h3 className="mb-2 font-medium">Write</h3>
             <Link
               href="mailto:rajnaman488@gmail.com"
               className="text-sm text-gray-600 hover:text-gray-900"
@@ -38,13 +38,13 @@ export default function Footer() {
 
         {/* Social Links Section */}
         <div>
-          <h3 className="font-medium mb-2">Follow</h3>
+          <h3 className="mb-2 font-medium">Follow</h3>
           <div className="flex gap-4">
             {socialLinks.map((social) => (
               <Link
                 key={social.name}
                 href={social.href}
-                className="text-gray-600 hover:text-gray-900 transition-colors"
+                className="text-gray-600 transition-colors hover:text-gray-900"
                 aria-label={social.name}
               >
                 <social.icon className="h-5 w-5" />
